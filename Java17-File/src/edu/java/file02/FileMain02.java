9bc12ce070d77a051cbd061a6a513a3417fefcae
crushon9 <catco929@naver.com>
2022-07-27 18:50:05 +0900

    end

---
+++ Java17-File/src/edu/java/file02/FileMain02.java
@@ -0,0 +1,53 @@
+package edu.java.file02;
+
+import java.io.*;
+
+// 프로그램 <=== FileInputStream <=== 파일
+// FileInputStream 클래스의 read() 메소드를 사용하여 파일을 읽음
+// 프로그램 ===> FileInputStream ===> 파일
+// FileInputStream 클래스의 write() 메소드를 사용하여 파일을 씀
+
+public class FileMain02 {
+
+	public static void main(String[] args) {
+		// temp/original.txt 파일에서 데이터를 읽어서
+		// temp/copy.txt 파일에서 데이터를 씀
+		InputStream in = null;
+		OutputStream out = null;
+
+		try { // 오류가 날수있기 때문에 무조건 예외처리하도록 되어있음
+				// 파일에서 데이터를 읽어올 통로인 FileInputStream 객체 생성
+			in = new FileInputStream("temp/original.txt");
+			// 파일에서 데이터를 쓸 통로인 FileInputStream 객체 생성
+			out = new FileOutputStream("temp/copy.txt");
+
+			int data = 0; // data = read() 메소드가 리턴하는 값을 저장할 변수
+			int byteCopied = 0; // write() 메소드를 호출할 때마다 1씩 증가
+			while (true) {
+				// read() : 파일에서 1바이트씩 데이터를 읽어옴
+				// 파일끝에 도달했을때 -1 리턴
+				data = in.read();
+				System.out.println("옮기는중" + data);
+				if (data == -1) {
+					break;
+				}
+
+				// write() : 1바이트씩 파일에 씀
+				out.write(data);
+				byteCopied++;
+			}
+			System.out.println(byteCopied + "바이트 복사됨");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
---
+++ Java17-File/src/edu/java/file03/FileMain03.java
@@ -0,0 +1,32 @@
+package edu.java.file03;
+
+/*
+ * 1. try-catch-finally 구문 (일반적인)
+ * try {
+ * 	실행문
+ * } catch (Exception e) {
+ * 	예외처리
+ * } finally {
+ * 	항상 실행할 코드 (=리소스닫기)
+ * }
+ */
+
+/*
+ * 2.  try-catch-resource 구문 (Java7부터 제공)
+ * : try( )안에서 생성된 리소스들의 해제 코드(close();)를 자동으로 호출해줌
+ * try (리소스생성) {
+ * 	실행문
+ * } catch (Exception e) {
+ * 	예외처리
+ * }
+ */
+
+
+public class FileMain03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
