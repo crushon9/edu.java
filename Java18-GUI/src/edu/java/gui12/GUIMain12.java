fe4029b25953d001cbce60595c3fd63c3af91716
crushon9 <catco929@naver.com>
2022-08-02 18:56:50 +0900

    gui end

---
+++ Java18-GUI/src/edu/java/gui12/GUIMain12.java
@@ -0,0 +1,84 @@
+package edu.java.gui12;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GUIMain12 {
+
+	private JFrame frame;
+
+	public JFrame getFrame() {
+		return frame;
+	}
+
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIMain12 window = new GUIMain12();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public JTextField getTextField() {
+		return textField;
+	}
+
+	public void setTextField(JTextField textField) {
+		this.textField = textField;
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUIMain12() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		setTextField(new JTextField());
+		getTextField().setBounds(12, 37, 408, 103);
+		frame.getContentPane().add(getTextField());
+		getTextField().setColumns(10);
+
+		JButton btnNewButton = new JButton("New Frame");
+		btnNewButton.setBounds(54, 170, 333, 68);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// textField 입력된 데이터를 myFrame - btnNewButton의 text에 적용
+				// 매개변수를 text로 해서 생성자에 넘겨서 처리하는 코드
+//				String text = getTextField().getText();
+//				MyFrame myFrame = new MyFrame(text);
+//				textField.setText(text); 
+
+				// 매개변수로 frame을 넘겨서 처리하는 코드
+				MyFrame myFrame = new MyFrame(frame);
+				myFrame.setVisible(true);
+			}
+		});
+		frame.getContentPane().add(btnNewButton);
+	}
+
+}
---
+++ Java18-GUI/src/edu/java/gui12/MyFrame.java
@@ -0,0 +1,39 @@
+package edu.java.gui12;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MyFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	public MyFrame(JFrame frame) { // 이게 생성자였어;; 매개변수도 가능하네?
+		// JFrame.EXIT_ON_CLOSE : 프로그램 전체 종료
+		// JFrame.DISPOSE_ON_CLOSE : 현재 창만 종료
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel(); // frame.getContentPane()과 동일
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		// 매개변수로 받은 frame을 getContentPane 하여, Component중 첫번재로 추가된 textField를 리턴
+		// 리턴한 값을 형변환하여 txtfd에 담는다
+		JTextField txtfd = (JTextField) frame.getContentPane().getComponent(0);
+
+		JButton btnNewButton = new JButton(txtfd.getText());
+		btnNewButton.setText(txtfd.getText());
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtfd.setText("안녕");
+			}
+		});
+		btnNewButton.setBounds(78, 80, 270, 115);
+		contentPane.add(btnNewButton);
+	}
+
+}
