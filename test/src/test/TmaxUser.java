473a0d88306281dcc50faa3cd25f046e2f181f85
crushon9 <catco929@naver.com>
2022-12-14 17:10:26 +0900

    test

--- test/src/test/TmaxUser.java
+++ test/src/test/TmaxUser.java
@@ -10,10 +10,10 @@ public class TmaxUser {
 //		int[] abnomal = { 1 };
 //		int[] result = { -1, 0, 20, 15, 55 };
 
-		int[] balance = { -1, 30, 30, 30, 30 };
-		int[][] transaction = { { 1, 2, 10 }, { 2, 3, 20 }, { 3, 4, 5 }, { 3, 4, 30 } };
-		int[] abnomal = { 2, 3 };
-		int[] result = { -1, 20, 0, 0, 40 };
+//		int[] balance = { -1, 30, 30, 30, 30 };
+//		int[][] transaction = { { 1, 2, 10 }, { 2, 3, 20 }, { 3, 4, 5 }, { 3, 4, 30 } };
+//		int[] abnomal = { 2, 3 };
+//		int[] result = { -1, 20, 0, 0, 40 };
 
 //		int[] balance = { -1, 40, 30, 50 };
 //		int[][] transaction = { { 1, 3, 10 }, { 2, 1, 10 }, { 3, 1, 45 }, { 2, 3, 10 }, { 1, 3, 35 }, { 2, 1, 5 },
@@ -21,10 +21,10 @@ public class TmaxUser {
 //		int[] abnomal = { 2 };
 //		int[] result = { -1, 40, 5, 45 };
 
-//		int[] balance = { -1, 100, 1, 1, 1, 1 };
-//		int[][] transaction = { { 1, 2, 100 }, { 2, 3, 101 }, { 3, 4, 102 }, { 4, 5, 103 }, { 5, 1, 104 } };
-//		int[] abnomal = { 1 };
-//		int[] result = { -1, 4, 0, 0, 0, 0 };
+		int[] balance = { -1, 100, 1, 1, 1, 1 };
+		int[][] transaction = { { 1, 2, 100 }, { 2, 3, 101 }, { 3, 4, 102 }, { 4, 5, 103 }, { 5, 1, 104 } };
+		int[] abnomal = { 1 };
+		int[] result = { -1, 4, 0, 0, 0, 0 };
 
 		// solution의 결과값 담기
 		int[] myResult = solution(balance, transaction, abnomal);
@@ -75,7 +75,7 @@ public class TmaxUser {
 					tranMatrix[sender][userTran][1] -= tranBal;
 					tranBal = 0;
 				} else {
-					// 문제는 여기서 덮어씌워지네,,, arrayList put에 키밸류로 써야할것같아..
+					// 문제는 여기서 덮어씌워지네,,, arrayList put에 키밸류로 써야할것                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   같아..
 					tranMatrix[receiver][userTran + 1][0] = tranMatrix[sender][userTran][0];
 					tranMatrix[receiver][userTran + 1][1] = senderLastBal;
 					tranMatrix[sender][userTran][1] = 0;
@@ -96,7 +96,7 @@ public class TmaxUser {
 		for (int user = 1; user < balance.length; user++) {
 			int balanceSum = 0;
 			for (int tran = 0; tran < transaction.length + 1; tran++) {
-				// 이상 유저인지 검사
+				// 이상 유저인지 검사9
 				int flag = 0;
 				for (int abUser : abnomal) {
 					if (tranMatrix[user][tran][0] == abUser) {
